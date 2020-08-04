150c52971afa5abf50423073561df374297c66b6
Dmitry <jathpr@gmail.com>
2020-08-04 20:03:40 +0300

    test files

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const indexToValue = (_,i)=>i
 const months = new Array(12).fill(0).map(indexToValue)
 
+
 months.forEach( (month)=> {
 const date = new Date(2009, month, 10); 
 const monthString = date.toLocaleString('default', { month: 'long' });
