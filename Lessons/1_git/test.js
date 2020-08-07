da3da6aac7b953792e9fd9e6f0e8007446d5ebe9
Dmitry <jathpr@gmail.com>
2020-08-07 16:52:21 +0300

    add React

--- Lessons/1_git/test.js
+++ Lessons/1_git/test.js
@@ -1,9 +1,8 @@
-const indexToValue = (_,i)=>i
-const monthsNew = new Array(12).fill(0).map(indexToValue)
+const indexToValue = (_, i) => i;
+const monthsNew = new Array(12).fill(0).map(indexToValue);
 
-monthsNew.forEach( (month)=> {
-const date = new Date(2009, month, 10); 
-const monthString = date.toLocaleString('default', { month: 'long' });
-console.log(monthString);
-}
-)
+monthsNew.forEach((month) => {
+  const date = new Date(2009, month, 10);
+  const monthString = date.toLocaleString("default", { month: "long" });
+  console.log(monthString);
+});
