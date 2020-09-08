38abf6c4e6249d1641b18d9b2c3822f135cae775
Dmitry <jathpr@gmail.com>
2020-09-08 18:39:41 +0300

    useReducer

--- react_box/src/Components/App.jsx
+++ react_box/src/Components/App.jsx
@@ -1,15 +1,26 @@
-import React, { useContext, useState } from 'react'
-import { ThemeContext, themes } from '../Styles/Themes'
-import { Home } from '../Pages'
+import React, { useReducer } from 'react'
 
 export const App = () => {
-	const [theme, setTheme] = useState(themes.dark)
+	const initialState = { count: 0 }
+
+	function reducer(state, action) {
+		switch (action.type) {
+			case 'increment':
+				return { count: state.count + 1 }
+			case 'decrement':
+				return { count: state.count - 1 }
+			default:
+				throw new Error()
+		}
+	}
+
+	const [state, dispatch] = useReducer(reducer, initialState)
 
 	return (
-		<ThemeContext.Provider value={theme}>
-			<Home />
-			<button onClick={() => setTheme(themes.dark)}>setDark</button>
-			<button onClick={() => setTheme(themes.light)}>setLight</button>
-		</ThemeContext.Provider>
+		<>
+			Count: {state.count}
+			<button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+			<button onClick={() => dispatch({ type: 'increment' })}>+</button>
+		</>
 	)
 }
